972f80852e3d7ad44160e9d548ddfc16e37a3e44
kokodrillo03 <krzychu.biska2@gmail.com>
2023-06-18 11:13:28 +0200

    lol

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-"""Flask"""
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    '''Index'''
-    return render_template('index.html')
+ return '<h1>Hello WSB! Greetings from Flask & Docker!</h1>'
 
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    '''hello's'''
-    return render_template('hello.html', name=name)
+if __name__ == "__main__":
+ app.run(debug=True)
